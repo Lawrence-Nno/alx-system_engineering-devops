e98ec8478568cb7bcae5f0aae16eaa09fecb0134
Lawrence Nno <Lawrence.Nno@gmail.com>
2023-04-25 00:10:18 +0100

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    done = [t.get("title") for t in todos if t.get("done") is True]
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(done), len(todos)))
-    [print("\t {}".format(d)) for d in done]
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
