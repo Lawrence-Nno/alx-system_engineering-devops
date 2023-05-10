df948b3e152b84b38e05f2cc368dc76adf8a9369
Lawrence Nno <lawrence.nno@gmail.com>
2023-05-10 03:39:08 +0100

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,7 +4,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """This func prints the titles of the 10 hottest posts on a given subreddit."""
+    """This prints the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
