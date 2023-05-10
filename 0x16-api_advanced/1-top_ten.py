b2f4250dc669c7163d89cc6bb6aa6cf56c1c4c08
Lawrence Nno <lawrence.nno@gmail.com>
2023-05-10 03:29:42 +0100

    Added Task 1 and 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-"""This function queries a list of all hot posts on a given Reddit subreddit."""
+"""This function prints hot posts on a given Reddit subreddit."""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """This returns a list of titles of all hot posts on a given subreddit."""
+def top_ten(subreddit):
+    """This prints the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     parameters = {
-        "after": after,
-        "count": count,
-        "limit": 100
+        "limit": 10
     }
     respons = requests.get(url, headers=headers, parameters=parameters,
                             allow_redirects=False)
     if respons.status_code == 404:
-        return None
-
+        print("None")
+        return
     outcome = respons.json().get("data")
-    after = outcome.get("after")
-    count += outcome.get("dist")
-    for c in outcome.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    [print(c.get("data").get("title")) for c in outcome.get("children")]
