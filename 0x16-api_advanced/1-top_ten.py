441e4abbda3ab9d97e628bb8f77abb3e95094cb0
Lawrence Nno <lawrence.nno@gmail.com>
2023-05-10 03:36:13 +0100

    TAsk 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""This function prints hot posts on a given Reddit subreddit."""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """This prints the titles of the 10 hottest posts on a given subreddit."""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    param = {
+    params = {
         "limit": 10
     }
-    respons = requests.get(url, headers=headers, param=param,
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if respons.status_code == 404:
+    if response.status_code == 404:
         print("None")
         return
-    outcome = respons.json().get("data")
-    [print(c.get("data").get("title")) for c in outcome.get("children")]
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
